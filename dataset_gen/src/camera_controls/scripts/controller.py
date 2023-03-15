7ec5c2235e4ae398be3a807a95c83aebcf7f723a
proitshnik <mister.kalmak@yandex.ru>
2023-03-15 19:00:27 +0300

    Add files related to the message type and change order of arguments in controller

--- dataset_gen/src/camera_controls/scripts/controller.py
+++ dataset_gen/src/camera_controls/scripts/controller.py
@@ -3,7 +3,7 @@ import sys
 import rospy
 import roslib
 from std_msgs.msg import String
-from camera_controls.msg import data
+from camera_controls.msg import msg_transposition
 
 
 def my_is_digit(string):
@@ -19,7 +19,7 @@ def my_is_digit(string):
 
 if __name__ == '__main__':
     try:
-        pub = rospy.Publisher('drivercontroller', data, queue_size=1)
+        pub = rospy.Publisher('drivercontroller', msg_transposition, queue_size=1)
         rospy.init_node('dronecontroller')
         while not rospy.is_shutdown():
             command = input()
@@ -34,9 +34,12 @@ if __name__ == '__main__':
                         command_list[0] == 'rotate' and len(command_list) == 5 and all(my_is_digit(x) for x in command_list[2:]) or \
                         command_list[0] == 'translate' and len(command_list) == 8 and all(my_is_digit(x) for x in command_list[2:]):
                     commands = [float(i) for i in command_list[2:]]
-                    commands.extend([float(0), ] * (6 - len(commands)))
-                    x, y, z, yaw, pitch, roll = commands
-                    pub.publish(x, y, z, yaw, pitch, roll)
+                    if command_list[0] == 'move':
+                        commands.extend([float(0), ] * 3)
+                    elif command_list[0] == 'rotate':
+                        commands = [float(0), ] * 3 + commands
+                    x, y, z, roll, pitch, yaw = commands
+                    pub.publish(x, y, z, roll, pitch, yaw)
                 else:
                     print("Check arguments!")
     except rospy.ROSInterruptException:
