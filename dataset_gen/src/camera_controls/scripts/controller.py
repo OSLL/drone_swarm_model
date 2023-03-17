3e746ec31609c8d43df3a7936fedd81b284f4ee0
proitshnik <mister.kalmak@yandex.ru>
2023-03-17 11:45:41 +0300

    Add taking odometry in controller

--- dataset_gen/src/camera_controls/scripts/controller.py
+++ dataset_gen/src/camera_controls/scripts/controller.py
@@ -4,6 +4,8 @@ import rospy
 import roslib
 from std_msgs.msg import String
 from camera_controls.msg import msg_transposition
+from gazebo_msgs.srv import GetModelState
+from tf.transformations import euler_from_quaternion
 
 
 def my_is_digit(string):
@@ -15,6 +17,22 @@ def my_is_digit(string):
             return True
         except ValueError:
             return False
+
+
+def odometry_client(model_name, relative_entity_name):
+    odom = '/gazebo/get_model_state'
+    rospy.wait_for_service(odom)
+    try:
+        odom_s = rospy.ServiceProxy(odom, GetModelState)
+        model_state = odom_s(model_name, relative_entity_name)
+        position = model_state.pose.position
+        orientation = euler_from_quaternion([model_state.pose.orientation.x,
+                                             model_state.pose.orientation.y,
+                                             model_state.pose.orientation.z,
+                                             model_state.pose.orientation.w])
+        return position, orientation
+    except rospy.ServiceException as e:
+        print(f"Service call failed: {e}")
 
 
 if __name__ == '__main__':
@@ -38,6 +56,10 @@ if __name__ == '__main__':
                     elif command_list[0] in ['rotate', 'rotate_direct']:
                         commands = [float(0), ] * 3 + commands
                     x, y, z, roll, pitch, yaw = commands
+                    model_name = command_list[1]
+                    position, orientation = odometry_client(model_name, '')
+                    print(position)
+                    print('rpy:', orientation)
                     topic_name = command_list[1] + "_driver"
                     pub = rospy.Publisher(topic_name, msg_transposition, queue_size=1)
                     if command_list[0] in ['move_direct', 'rotate_direct', 'translate_direct']:
