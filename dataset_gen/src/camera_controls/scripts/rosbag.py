0e111a31a310292734887661387108fb7b81951b
proitshnik <mister.kalmak@yandex.ru>
2023-04-12 22:59:49 +0300

    Add template and record protection to method to start and stop record

--- dataset_gen/src/camera_controls/scripts/rosbag.py
+++ dataset_gen/src/camera_controls/scripts/rosbag.py
@@ -3,19 +3,29 @@ import sys
 import re
 import rospy
 import roslib
+import rosbag
 from std_msgs.msg import String
 from camera_controls.msg import msg_transposition
 from gazebo_msgs.srv import GetModelState
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_multiply, quaternion_conjugate
 
 
-def processing(data):
-    pass
+recording_status = False
+
+
+def processing(data, topic_name):
+    global recording_status
+    if data.data == "start record" and not recording_status:
+        recording_status = True
+        print("start record ", topic_name)
+    elif data.data == "stop record":
+        recording_status = False
+        print("stop record ", topic_name)
 
 
 def listener(topic_name):
     rospy.init_node('rosbaglistener')
-    rospy.Subscriber(topic_name, msg_transposition, processing)
+    rospy.Subscriber("rosbag_command", String, processing, topic_name)
     rospy.spin()
 
 
