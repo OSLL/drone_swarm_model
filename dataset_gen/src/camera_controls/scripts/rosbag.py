45e216e8988e2592a7c6df848be95170f8d0138c
proitshnik <mister.kalmak@yandex.ru>
2023-04-13 22:49:36 +0300

    Add start and stop functional, support record multi files sequentially. Transform to class RosbagProcess

--- dataset_gen/src/camera_controls/scripts/rosbag.py
+++ dataset_gen/src/camera_controls/scripts/rosbag.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import re
+import subprocess
+import signal
+import os
 import rospy
 import roslib
 import rosbag
@@ -10,27 +13,45 @@ from gazebo_msgs.srv import GetModelState
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_multiply, quaternion_conjugate
 
 
-recording_status = False
+class RosbagProcess:
+    def __init__(self, topic_name):
+        self.topic_name = topic_name
+        self.process = None
+        self.recording_status = False
+        self.count = 0
 
+    def processing(self, data):
+        if data.data == "start record" and not self.recording_status:
+            self.recording_status = True
+            command = f"rosbag record -O rosbag{self.count}.bag /{self.topic_name}"
+            print(command)
+            self.process = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True)
+            print("start record ", self.topic_name)
+        elif data.data == "stop record" and self.recording_status:
+            self.recording_status = False
+            self.stop_process(self.process)
+            self.count += 1
+            print("stop record ", self.topic_name)
 
-def processing(data, topic_name):
-    global recording_status
-    if data.data == "start record" and not recording_status:
-        recording_status = True
-        print("start record ", topic_name)
-    elif data.data == "stop record":
-        recording_status = False
-        print("stop record ", topic_name)
+    def stop_process(self, process):
+        cmd = subprocess.Popen(f"ps -o pid --ppid {process.pid} --noheaders", stdout=subprocess.PIPE, shell=True)
+        cmd_out = cmd.stdout.read()
+        code = cmd.wait()
+        assert code == 0, f"Cmd return {code}"
+        cmd_out = cmd_out.decode('utf-8')
+        for pid in cmd_out.split("\n")[:-1]:
+            os.kill(int(pid), signal.SIGINT)
+        process.terminate()
 
-
-def listener(topic_name):
-    rospy.init_node('rosbaglistener')
-    rospy.Subscriber("rosbag_command", String, processing, topic_name)
-    rospy.spin()
+    def listener(self):
+        rospy.init_node('rosbaglistener')
+        rospy.Subscriber("rosbag_command", String, self.processing)
+        rospy.spin()
 
 
 if __name__ == '__main__':
     try:
-        listener(sys.argv[1])
+        rosbag = RosbagProcess(sys.argv[1])
+        rosbag.listener()
     except (rospy.ROSInterruptException, IndexError):
         print("Check argument!")
