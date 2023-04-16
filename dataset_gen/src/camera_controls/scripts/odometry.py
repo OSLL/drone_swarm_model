32f04c445ab45b6e59234c5e633773ff39993622
proitshnik <mister.kalmak@yandex.ru>
2023-04-16 18:29:01 +0300

    Add odometry publisher per second for all drones in topics drone_name/odom

---
+++ dataset_gen/src/camera_controls/scripts/odometry.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import time
+import rospy
+import roslib
+from camera_controls.msg import msg_transposition
+from camera_controls.msg import msg_odometry
+from gazebo_msgs.srv import GetModelState, GetWorldProperties
+
+
+def odometry_processing():
+    drone_names = []
+    try:
+        world_s = rospy.ServiceProxy('gazebo/get_world_properties', GetWorldProperties)
+        properties = world_s()
+        if properties.success:
+            drone_names = list(filter(lambda x: 'drone' in x, properties.model_names))
+        else:
+            print("Simulation is not running!")
+    except rospy.ServiceException as e:
+        print("Simulation is not running!")
+    odom = '/gazebo/get_model_state'
+    rospy.wait_for_service(odom)
+    try:
+        odom_s = rospy.ServiceProxy(odom, GetModelState)
+        for drone_name in drone_names:
+            drone_state = odom_s(drone_name, "")
+            orientation = drone_state.pose.orientation
+            pub = rospy.Publisher(drone_name + "/odom", msg_odometry, queue_size=1)
+            pub.publish(orientation.x, orientation.y, orientation.z, orientation.w)
+    except rospy.ServiceException as e:
+        print(f"Service call failed: {e}")
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('odometrypublisher')
+        while not rospy.is_shutdown():
+            odometry_processing()
+            time.sleep(1)
+    except rospy.ROSInterruptException:
+        pass
